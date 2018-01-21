fae36c08085265b1078e5add458d6d07f18e56ed
adadesions <adadesions@gmail.com>
2018-01-21 09:51:57 +0700

    create view and url for cv pages

--- resumeSite/resumeApp/urls.py
+++ resumeSite/resumeApp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('<str:template_no>/', views.templates, name='templates'),
+    path('cv/<str:cv_lang>', views.templates, name='cv'),
     path('to_pdf/<str:template_no>/', views.to_pdf, name='to_pdf'),
 ]
--- resumeSite/resumeApp/views.py
+++ resumeSite/resumeApp/views.py
@@ -4,17 +4,18 @@ from django.template.loader import render_to_string
 from django.core.files.storage import FileSystemStorage
 from weasyprint import HTML
 
+template_dir = 'resumeApp/resumeTemplate'
 
 def templates(request, template_no):
     try:
-        respones_file = ''.join(['resumeApp/resumeTemplate', template_no, '.html'])
+        respones_html = ''.join([template_dir, template_no, '.html'])
         context = {'template_no': 1} 
-        return render(request, respones_file, context)
+        return render(request, respones_html, context)
     except TemplateDoesNotExist:
         return redirect(templates, template_no=1)
 
 def to_pdf(request, template_no):
-    html_file = ''.join(['resumeApp/resumeTemplate', template_no, '.html'])
+    html_file = ''.join([template_dir, template_no, '.html'])
     html_string = render_to_string(html_file)
     html = HTML(string=html_string)
     html.write_pdf(target="/tmp/mypdf.pdf")
@@ -24,3 +25,12 @@ def to_pdf(request, template_no):
         response = HttpResponse(pdf, content_type='application/pdf')
         response['Content-Disposition'] = 'attachment; filename="mypdf.pdf"'
     return response
+
+
+def cv(request, cv_lang):
+    if cv_lang == 'en':
+        respons_html = ''.join(['resumeApp/cv_eng.html'])
+    else 
+        respons_html = ''.join(['resumeApp/cv_thai.html'])
+    
+    return render(request, respons_html)
