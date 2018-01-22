3e661fdfbd91a7a0cf843b19f615ef794faa24fd
adadesions <adadesions@gmail.com>
2018-01-22 17:13:43 +0700

    add css_dir

--- resumeSite/resumeApp/views.py
+++ resumeSite/resumeApp/views.py
@@ -11,7 +11,7 @@ template_dir = 'resumeApp/resumeTemplate'
 def templates(request, template_no):
     try:
         respones_html = ''.join([template_dir, template_no, '.html'])
-        context = {'template_no': template_no} 
+        context = {'template_no': template_no}
         return render(request, respones_html, context)
     except TemplateDoesNotExist:
         return redirect(templates, template_no=1)
@@ -23,10 +23,12 @@ def to_pdf(request, template_no):
     '''
 
     html_file = ''.join([template_dir, template_no, '.html'])
+    css_dir = os.getcwd() + '/resumeApp/static/css
     html_string = render_to_string(html_file, {'is_pdf_view': True})
     html = HTML(string=html_string, base_url=request.build_absolute_uri())
     css = [
-        CSS('/home/adadesions/Githubs/ResumeProject/resumeSite/resumeApp/static/css/screens/template'+template_no+'_screen.css'),
+        CSS(css_dir + '/screens/common_style.css'),
+        CSS(css_dir + '/screens/template'+template_no+'_screen.css'),
     ]
     pdf_file = html.write_pdf(stylesheets=css)
     response = HttpResponse(pdf_file, content_type='application/pdf')
