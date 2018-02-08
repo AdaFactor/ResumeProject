43ca57cb395200fc01e06ed22225b0f45f623c36
sirimainson <sirimainson@gmail.com>
2018-02-08 14:57:24 +0700

    update plus button

--- resumeSite/resumeApp/forms.py
+++ resumeSite/resumeApp/forms.py
@@ -1,5 +1,5 @@
 from django.forms import ModelForm, DateInput, modelformset_factory
-from .models import Student, Letter, Experience
+from .models import Student, Letter, Experience, Education, Reference, Language, Skill
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Submit, Layout, Fieldset, Field, Div, Button
 from crispy_forms.bootstrap import PrependedText, PrependedAppendedText, FormActions, StrictButton, InlineField
@@ -101,23 +101,19 @@ class StudentForm(ModelForm):
                         'email',
                         'address_en',
                         'education',
-                        Div(
-                            'education',
-                            css_id='dialog-education'
-                        ),
                         FormActions(
-                            Button('add', '+ Add Education', css_class="btn-primary",
-                                data_dismiss="modal", aria_hidden="true"),
+                            Button('education', '+ Add Education', css_class="btn-primary",
+                                data_toggle="modal", data_target="#myEducation"),
                         ),
                         'reference',
                         FormActions(
                             Button('add', '+ Add Reference', css_class="btn-primary",
-                                data_dismiss="modal", aria_hidden="true"),
+                                data_toggle="modal", data_target="#myReference"),
                         ),
                         'language',
                         FormActions(
                             Button('add', '+ Add Language', css_class="btn-primary",
-                                data_dismiss="modal", aria_hidden="true"),
+                                data_toggle="modal", data_target="#myLanguage"),
                         ),
                     css_class='col-lg-5'
                     ),
@@ -128,19 +124,20 @@ class StudentForm(ModelForm):
                         'skill',
                         FormActions(
                             Button('add', '+ Add Skill', css_class="btn-primary",
-                                data_dismiss="modal", aria_hidden="true"),
+                                data_toggle="modal", data_target="#mySkill"),
                         ),
                         'experience',
                         FormActions(
                             Button('add', '+ Add Experience', css_class="btn-primary",
-                                data_dismiss="modal", aria_hidden="true"),
+                                data_toggle="modal", data_target="#myExperience"),
                         ),
                         'activity',
                         'hobby',
                         css_class='col-lg-6')
                     ,css_class='row-fluid'
                 ),
-                Submit('save', 'Save', css_class='btn btn-success btn-lg btn-block')
+                Submit('save', 'Save', css_class='btn btn-success btn-lg btn-block'),
+                css_id="student-form",
                 ),    
             )
 
@@ -187,37 +184,157 @@ class LetterForm(ModelForm):
                     'company_name',
                     Field('contents', placeholder="Use # if you want a paragraph break (a new paragraph)"),
                 ),
-                Submit('save', 'Save', css_class='btn btn-success btn-block')  
+                Submit('save', 'Save', css_class='btn btn-success btn-block'),
+                css_id='letter-form'  
             ),            
         )
 
-# class EducationForm(ModelForm):
-#     class Meta:
-#         model = Education
-#         field = [
-#             'academy_name',
-#             'level',
-#             'major',
-#             'time_period',
-#         ]
+class EducationForm(ModelForm):
+    class Meta:
+        model = Education
+        fields = [
+            'academy_name',
+            'level',
+            'major',
+            'time_period',
+        ]
     
-#     def __init__(self, request, *args, **kwargs):
-#         super(EducationForm, self).__init__(*args, **kwargs)
-#         self.helper = FormHelper()
-#         self.helper.form_method = 'POST'
-#         self.helper.form_action = 'cv'        
-#         self.helper.form_class = 'form-horizontal'
-#         self.helper.label_class = 'col-lg-2'
-#         self.helper.field_class = 'col-lg-6'
-#         self.helper.layout = Layout(
-#             Fieldset(
-#                 'Education',
-#                 Field(
-#                     'academy_name',
-#                     'level',
-#                     'major',
-#                     'time_period',
-#                 )
-#             )
-        # )
+    def __init__(self, request, *args, **kwargs):
+        super(EducationForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'cv'        
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Education',
+                Field(
+                    'academy_name',
+                    'level',
+                    'major',
+                    'time_period',
+                ),
+                css_id='education-form',                  
+            ),
+        )
         
+class ReferenceForm(ModelForm):
+    class Meta:
+        model = Reference
+        fields = [
+            'advisor_name',
+            'position',
+            'affiliation',
+            'phone_no',
+            'email',
+        ]
+
+    def __init__(self, request, *args, **kwargs):
+        super(ReferenceForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'cv'        
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Reference',
+                Field(
+                    'advisor_name',
+                    'position',
+                    'affiliation',
+                    'phone_no',
+                    'email',
+                ),
+                css_id='reference-form',                                                    
+            ),
+        )
+
+class LanguageForm(ModelForm):
+    class Meta:
+        model = Language
+        fields = [
+            'name',
+            'level',
+        ]
+
+    def __init__(self, request, *args, **kwargs):
+        super(LanguageForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'cv'        
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Language',
+                Field(
+                    'name',
+                    'level',
+                ),
+                css_id='language-form',                         
+            ),
+        )
+
+class SkillForm(ModelForm):
+    class Meta:
+        model = Skill
+        fields = [
+            'name',
+            'level',
+        ]
+
+    def __init__(self, request, *args, **kwargs):
+        super(SkillForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'cv'        
+        self.helper.form_class = 'form-horizontal'
+        self.helper.label_class = 'col-lg-2'
+        self.helper.field_class = 'col-lg-6'
+        self.helper.layout = Layout(
+            Fieldset(
+                'Skill',
+                Field(
+                    'name',
+                    'level',
+                ),
+                css_id='skill-form',                                  
+            ),
+        )
+
+class ExperienceForm(ModelForm):
+    class Meta:
+        model = Experience
+        fields = [
+            'company_name',
+            'position',
+            'role',
+            'time_period',
+        ]
+    
+    def __init__(self, request, *args, **kwargs):
+            super(Experience, self).__init__(*args, **kwargs)
+            self.helper = FormHelper()
+            self.helper.form_method = 'POST'
+            self.helper.form_action = 'cv'        
+            self.helper.form_class = 'form-horizontal'
+            self.helper.label_class = 'col-lg-2'
+            self.helper.field_class = 'col-lg-6'
+            self.helper.layout = Layout(
+                Div (
+                    Fieldset (
+                        'Experiences',
+                        Field (
+                            'company_name',
+                            'position',
+                            'role',
+                            'time_period',
+                        ),
+                    ),           
+                ),  
+            )
