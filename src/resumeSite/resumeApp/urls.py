a260fdc84e53f429bac5285f9d64e5e792819c47
adadesions <adadesions@gmail.com>
2018-04-04 02:06:38 +0700

    Fix button in view resume

--- src/resumeSite/resumeApp/urls.py
+++ src/resumeSite/resumeApp/urls.py
@@ -6,7 +6,11 @@ urlpatterns = [
     path('<str:template_no>/', views.templates, name='templates'),
     path('cv/<str:cv_lang>/<int:cv_id>', views.cv, name='cv'),
     path('to_pdf/<str:template_no>/', views.to_pdf, name='to_pdf'),
-    path('to_pdf_cv/<str:cv_lang>/<int:cv_id>', views.to_pdf_cv, name='to_pdf_cv'),
+    path(
+        'to_pdf_cv/<str:cv_lang>/<int:cv_id>',
+        views.to_pdf_cv,
+        name='to_pdf_cv'
+    ),
     path('view/<str:doc_type>', views.view_doc, name='view_doc'),
     path('new/<str:doc_type>', views.new_doc, name='new_doc'),
     path('delete/cv/<int:cv_id>', views.delete_cv, name='delete_cv'),
